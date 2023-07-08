4145a2379e99e5c8e5f4ca642bf881cf0aaed99e
Usman123 <um7995367@gmail.com>
2023-07-08 21:07:53 +0500

    Allowing the selection of items in the list

--- Course proj/proj-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Course proj/proj-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  OnDestroy,
   OnInit,
   // ElementRef,
   // ViewChild
@@ -8,19 +9,31 @@ import {
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit , OnDestroy{
   // @ViewChild('nameInput', { static: false }) nameInputRef!: ElementRef;
   // @ViewChild('amountInput', { static: false }) amountInputRef!: ElementRef;
-
+ subscription!: Subscription;
+ editMode = false;
+ editedItemIndex!: number;
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+    this.subscription = this.slService.startedEditting.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onAddItem(form: NgForm) {
--- Course proj/proj-app/src/app/shopping-list/shopping-list.service.ts
+++ Course proj/proj-app/src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditting = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
