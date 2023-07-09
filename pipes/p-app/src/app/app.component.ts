6a2569ceeab8dba3b6677c0ba4905cb07ba12f48
Usman123 <um7995367@gmail.com>
2023-07-09 18:08:58 +0500

    pure and impure pipes

--- pipes/p-app/src/app/app.component.ts
+++ pipes/p-app/src/app/app.component.ts
@@ -40,4 +40,12 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+  onAddSerevr(){
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Serevr',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    });
+  }
 }
