7a2208d2a672a22ac970f847cd1fc3e3399dd899
Usman123 <um7995367@gmail.com>
2023-07-09 17:12:29 +0500

    creating a filter pipe

---
+++ pipes/p-app/src/app/filter.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterString: string, propName: string): any {
+    if (value.length === 0 || filterString === '' ){
+      return value;
+    }
+    const ResultArray = [];
+    for (const item of value){
+      
+      if (item[propName] === filterString){
+        ResultArray.push(item);
+      }
+      
+    }
+    return ResultArray;
+  }
+
+}
