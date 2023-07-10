e6fb464edcde4747bec036b49b75442c0f1a2398
Usman123 <um7995367@gmail.com>
2023-07-10 14:01:04 +0500

    Getting data

--- pipes/p-app/src/app/app.component.ts
+++ pipes/p-app/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -24,9 +26,15 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+  private fetchPosts(){
+    this.http.get('https://ng-template-1-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json').subscribe(posts => {
+      console.log(posts);
+    })
   }
 }
