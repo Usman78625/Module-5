6dd8c8227b74fc205c645b0efa6fdbaa33b50a02
Usman123 <um7995367@gmail.com>
2023-07-12 16:00:58 +0500

    Transforming response data

--- pipes/p-app/src/app/shared/data-storage.service.ts
+++ pipes/p-app/src/app/shared/data-storage.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 // import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -19,9 +20,18 @@ storeRecipes( ){
 }
 
 fetchRecipes(){
-    this.http.get<Recipe[]>('https://ng-course-project-d9f93-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json').subscribe(recipes =>{
-        this.recipeService.setRecipes(recipes)
-    })
+    this.http.get<Recipe[]>('https://ng-course-project-d9f93-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json').pipe(
+        map(recipes =>{
+            return recipes.map(recipe =>{
+                return{
+                    ...recipe,
+                    ingredients: recipe.ingredients ? recipe.ingredients : []
+                };
+            });
+        }))
+        .subscribe(recipess =>{
+            this.recipeService.setRecipes(recipess)
+        })
+    
 }
-
-}
+ }
